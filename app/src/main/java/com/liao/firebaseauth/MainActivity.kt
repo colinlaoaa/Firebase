8ae46874d33b224db458e7815f8c03b3628b887a
Liao <huliao411@gmail.com>
2020-07-28 18:11:19 -0500

    fixed small bug, minimize the size of layout and activity

--- app/src/main/java/com/liao/firebaseauth/MainActivity.kt
+++ app/src/main/java/com/liao/firebaseauth/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.liao.firebaseauth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login1.*
@@ -39,13 +36,32 @@ class MainActivity : AppCompatActivity() {
             button_register.visibility = View.GONE
             button_forget_password.visibility = View.VISIBLE
 
-        }
-        if (button_change_email.visibility == View.GONE) {
+
             button_forget_password.setOnClickListener {
                 var email2 = edit_text_email.text.toString()
-                user!!.updateEmail(email2)
+                user.updateEmail(email2).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Email Updated to $email2",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "Email Updated Failed",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                }
             }
+
         }
+
+
+
 
 
 
@@ -56,33 +72,52 @@ class MainActivity : AppCompatActivity() {
             button_login.visibility = View.GONE
             button_register.visibility = View.GONE
             button_forget_password.visibility = View.VISIBLE
-
-        }
-        if (button_change_password2.visibility == View.GONE) {
             button_forget_password.setOnClickListener {
                 var password2 = edit_text_password.text.toString()
-                user!!.updatePassword(password2)
+                println(password2)
+                user.updatePassword(password2)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                applicationContext,
+                                "Password Updated to $password2",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            finish()
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                "Password Updated Failed",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        }
+                    }
             }
+
         }
+
+
+
 
 
         button_sent_reset_email.setOnClickListener {
-            auth.sendPasswordResetEmail(user?.email!!)
+            auth.sendPasswordResetEmail(user.email!!)
             Toast.makeText(applicationContext, "success, check your mailbox", Toast.LENGTH_SHORT)
                 .show()
         }
 
         button_delete_user.setOnClickListener {
-            user!!.delete()
+            user.delete()
             Toast.makeText(applicationContext, "delete success", Toast.LENGTH_SHORT)
                 .show()
             auth.signOut()
-            startActivity(Intent(this,LoginActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
         }
 
         button_sign_out.setOnClickListener {
             auth.signOut()
-            startActivity(Intent(this,LoginActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
         }
 
 
